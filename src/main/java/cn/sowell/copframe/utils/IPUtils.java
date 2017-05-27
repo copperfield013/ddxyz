4aa08e15772e23ad5838d8c7134145dfcdd6aa3b
copperfield <copperfield@9afdef4e-c05b-bb4b-bdb9-2143b111bd26>
2017-05-27 03:24:14 +0000

    git-svn-id: http://47.100.187.235:13932/svn/ddxyz@92 9afdef4e-c05b-bb4b-bdb9-2143b111bd26

--- src/main/java/cn/sowell/copframe/utils/IPUtils.java
+++ src/main/java/cn/sowell/copframe/utils/IPUtils.java
@@ -1,7 +1,15 @@
 package cn.sowell.copframe.utils;
 
 import sun.net.util.IPAddressUtil;
-
+/**
+ * 
+ * <p>Title: IPUtils</p>
+ * <p>Description: </p><p>
+ * 如果sun.net.util.IPAddressUtil的包提示不存在，那么需要配置eclipse的buildPath，添加一个accessible的条目sun/**
+ * </p>
+ * @author Copperfield Zhang
+ * @date 2017年5月27日 上午11:21:15
+ */
 public class IPUtils {
 	/**
 	 * 判断一个ip地址是否是内网地址
