a54d52b420a3d327b420026d87cf8ee377548b32
copperfield <copperfield@9afdef4e-c05b-bb4b-bdb9-2143b111bd26>
2017-06-02 02:05:02 +0000

    git-svn-id: http://47.100.187.235:13932/svn/ddxyz@98 9afdef4e-c05b-bb4b-bdb9-2143b111bd26

--- src/test/java/test/sowell/ddxyz/common/core/UtilTest.java
+++ src/test/java/test/sowell/ddxyz/common/core/UtilTest.java
@@ -1,7 +1,12 @@
 package test.sowell.ddxyz.common.core;
 
-import org.junit.Test;
+import java.text.ParseException;
+import java.util.Date;
 
+import org.junit.Test;
+import org.quartz.impl.calendar.CronCalendar;
+
+import cn.sowell.copframe.dto.format.OfDateFormat;
 import cn.sowell.copframe.utils.text.TextHandler;
 
 public class UtilTest {
@@ -12,5 +17,13 @@ public class UtilTest {
 		handler.setParameter("a", "aaaa");
 		handler.setParameter("x", "xxx");
 		System.out.println(handler.trim("s").getText(true));
+	}
+	
+	@Test
+	public void cronTest() throws ParseException{
+		CronCalendar cal = new CronCalendar("* * 1 * * ?");
+		OfDateFormat df = new OfDateFormat();
+		Date date = df.parse("2017-6-3 1:00:01");
+		System.out.println(cal.isTimeIncluded(date.getTime()));
 	}
 }
